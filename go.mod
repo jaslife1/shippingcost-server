8ad7bb7fdf3f2d44b77b78260dcf8783a2a9cdf9
Jason Buhay <buhayjason@gmail.com>
2022-02-11 12:35:40 +0800

    Added CORS support in server

--- go.mod
+++ go.mod
@@ -4,12 +4,14 @@ go 1.17
 
 require (
 	github.com/99designs/gqlgen v0.15.1
+	github.com/go-chi/chi v1.5.4
+	github.com/gorilla/websocket v1.4.2
+	github.com/rs/cors v1.8.2
 	github.com/vektah/gqlparser/v2 v2.2.0
 )
 
 require (
 	github.com/agnivade/levenshtein v1.1.0 // indirect
-	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/hashicorp/golang-lru v0.5.0 // indirect
 	github.com/mitchellh/mapstructure v1.2.3 // indirect
 )
--- server.go
+++ server.go
@@ -6,9 +6,13 @@ import (
 	"os"
 
 	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/handler/transport"
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/jaslife1/shippingcost-server/graph"
 	"github.com/jaslife1/shippingcost-server/graph/generated"
+	"github.com/go-chi/chi"
+	"github.com/rs/cors"
+	"github.com/gorilla/websocket"
 )
 
 const defaultPort = "8080"
@@ -19,11 +23,37 @@ func main() {
 		port = defaultPort
 	}
 
-	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+	router := chi.NewRouter()
 
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+    // Add CORS middleware around every request
+    // See https://github.com/rs/cors for full option listing
+    router.Use(cors.New(cors.Options{
+        AllowedOrigins:   []string{"http://localhost:3000", "http://localhost:8080"},
+        AllowOriginFunc:  func(origin string) bool { return true },
+        AllowedMethods:   []string{},
+        AllowedHeaders:   []string{},
+        AllowCredentials: true,
+        Debug:            true,
+    }).Handler)
+
+    srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+    srv.AddTransport(&transport.Websocket{
+        Upgrader: websocket.Upgrader{
+            CheckOrigin: func(r *http.Request) bool {
+                // Check against your desired domains here
+                return r.Host == "localhost:8080"
+            },
+            ReadBufferSize:  1024,
+            WriteBufferSize: 1024,
+        },
+    })
+
+    router.Handle("/", playground.Handler("GraphQL playground", "/query"))
+    router.Handle("/query", srv)
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+    err := http.ListenAndServe(":"+port, router)
+    if err != nil {
+        panic(err)
+    }
 }
